6886cb3f27637ece7288f5e109124298223077e0
vinarangkuti <ervina.sahrati@yahoo.com>
2016-08-16 01:17:09 -0700

    Mencetak Tree secara PostOrder

--- PostOrder.cpp
+++ PostOrder.cpp
@@ -51,3 +51,10 @@ void inOrder(Node *root)
 void postOrder(Node *root)
 {
 
+	 if(root != NULL){
+            postOrder(root->kiri);
+            postOrder(root->kanan);
+            printf("%d ", root->data);
+      }
+}
+
